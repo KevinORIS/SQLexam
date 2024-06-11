24b99c9b3db52d44b0311cdf5fb5134484e1f421
User <User@User-PC>
2024-06-11 18:49:07 +0300

    Add Hospital class

---
+++ SQLExam/src/by/itstep/oris/workspace/model/entity/Hospital.java
@@ -0,0 +1,24 @@
+package by.itstep.oris.workspace.model.entity;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class Hospital implements Iterable<Patient> {
+	private List<Patient> patients;
+	
+	public Hospital() {
+        patients = new ArrayList<>();
+    }
+
+    public void addPatient(Patient patient) {
+        patients.add(patient);
+    }
+
+    @Override
+    public Iterator<Patient> iterator() {
+        return patients.iterator();
+    }
+
+}
