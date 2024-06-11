d4ec0801d9868ada1617e4a1d396b37cbb51fb98
User <User@User-PC>
2024-06-11 21:45:32 +0300

    Rework CRUD class

--- SQLExam/src/by/itstep/oris/workspace/dal/CRUD.java
+++ SQLExam/src/by/itstep/oris/workspace/dal/CRUD.java
@@ -1,9 +1,12 @@
 package by.itstep.oris.workspace.dal;
 
+import by.itstep.oris.workspace.model.entity.Hospital;
+import by.itstep.oris.workspace.model.entity.Patient;
+
 public interface CRUD {
-	void insert();
-	void delete();
-	void update();
-	void get();
-	void getAll();
+	void insert(Patient patient);
+	void delete(int id);
+	void update(int id, Patient patient);
+	Patient get(int id);
+	Hospital getAll();
 }
