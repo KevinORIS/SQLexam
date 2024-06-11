bf9d42a1c4a50f30e1de8cec1f1857eea9628795
User <User@User-PC>
2024-06-11 19:25:33 +0300

    Add AbstractDAO class

---
+++ SQLExam/src/by/itstep/oris/workspace/dal/AbstractDAO.java
@@ -0,0 +1,34 @@
+package by.itstep.oris.workspace.dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class AbstractDAO implements CRUD {
+	public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+	public static final String URL = "jdbc.mysql://localhost:3306/hospital";
+	public static final String LOGIN = "root";
+	public static final String PASSWORD = "mysql123";
+	
+	protected Connection connection;
+	
+	protected void connect() {
+		try {
+			Class.forName(DRIVER);
+			
+			connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+		} catch (ClassNotFoundException | SQLException e) {
+			System.out.println(e);
+		}
+	}
+	
+	protected void release() {
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch(SQLException e) {
+				System.out.print(e);
+			}
+		}
+	}
+}
